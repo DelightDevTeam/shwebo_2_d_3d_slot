1e85d1e781395f02c161dc21b0ddba33fa3c49be
sophia <thangneimawi@aceplussolutions.com>
2024-05-27 21:16:33 +0630

    fixed report

--- app/Http/Controllers/Api/V1/Player/WagerController.php
+++ app/Http/Controllers/Api/V1/Player/WagerController.php
@@ -2,11 +2,8 @@
 
 namespace App\Http\Controllers\Api\V1\Player;
 
-use App\Enums\TransactionStatus;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\SeamlessTransactionResource;
-use App\Http\Resources\WagerResource;
-use App\Models\SeamlessTransaction;
 use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
@@ -18,35 +15,38 @@ class WagerController extends Controller
 
     public function index(Request $request)
     {
-        $type = $request->get('type');
+        $type = $request->get("type");
 
         [$from, $to] = match ($type) {
-            'yesterday' => [now()->subDay()->startOfDay(), now()->subDay()->endOfDay()],
-            'this_week' => [now()->startOfWeek(), now()->endOfWeek()],
-            'last_week' => [now()->subWeek()->startOfWeek(), now()->subWeek()->endOfWeek()],
+            "yesterday" => [now()->subDay()->startOfDay(), now()->subDay()->endOfDay()],
+            "this_week" => [now()->startOfWeek(), now()->endOfWeek()],
+            "last_week" => [now()->subWeek()->startOfWeek(), now()->subWeek()->endOfWeek()],
             default => [now()->startOfDay(), now()],
         };
 
         $user = auth()->user();
-
-        $transactions = SeamlessTransaction::leftJoin('products', 'products.id', '=', 'seamless_transactions.product_id')
-            ->select(
-                DB::raw('MIN(seamless_transactions.created_at) as from_date'),
-                DB::raw('MAX(seamless_transactions.created_at) as to_date'),
+    
+        $transactions = $this->makeJoinTable()->select(
                 'products.name as product_name',
-                'user_id',
-                'product_id',
-                DB::raw('COUNT(product_id) as total_count'),
-                DB::raw('SUM(bet_amount) as total_bet_amount'),
-                DB::raw('SUM(transaction_amount) as total_transaction_amount')
-            )
-            ->where('seamless_transactions.status', TransactionStatus::Settle->value)
-            ->whereBetween('seamless_transactions.created_at', [$from, $to])
-            ->where('user_id', $user->id)
-            ->groupBy('user_id', 'product_id')
-            ->orderBy('products.name')
-            ->paginate();
-
+                DB::raw('MIN(reports.settlement_date) as from_date'),
+                DB::raw('MAX(reports.settlement_date) as to_date'),
+                DB::raw('COUNT(product_code) as total_count'),
+                DB::raw('SUM(reports.bet_amount) as total_bet_amount'),
+                DB::raw('SUM(reports.payout_amount) as total_payout_amount'))
+                ->groupBy('product_name')
+                ->where("member_name", $user->user_name)
+                ->whereBetween("reports.settlement_date", [$from, $to])
+                ->paginate();
         return $this->success(SeamlessTransactionResource::collection($transactions));
+    }
+
+    private function makeJoinTable()
+    {
+        $query = User::query();
+        $query->join('reports', 'reports.member_name', '=', 'users.user_name')
+            ->join('products', 'reports.product_code', '=', 'products.code')
+            ->where('reports.status', '101');
+
+        return $query;
     }
 }
