7c1fc7809b97e8318163d7f991ea17f4bffe462f
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-06-05 07:08:01 +0630

    a

--- app/Http/Controllers/Api/V1/ThreeD/OneWeekRecWihtSlipController.php
+++ app/Http/Controllers/Api/V1/ThreeD/OneWeekRecWihtSlipController.php
@@ -43,7 +43,6 @@ class OneWeekRecWihtSlipController extends Controller
                 ->whereBetween('lottery_three_digit_pivots.res_date', [$start_date, $end_date])
                 ->select('lottery_three_digit_pivots.user_id', 'lottos.slip_no', DB::raw('SUM(lottery_three_digit_pivots.sub_amount) as total_sub_amount'))
                 ->groupBy('lottery_three_digit_pivots.user_id', 'lottos.slip_no')
-                ->latest()
                 ->get();
 
             // Calculate the total amount from the lottos table for the authenticated user within the date range
