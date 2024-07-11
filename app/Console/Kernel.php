23ab5dd42be438acd6615acbe304e3087b4f3c3f
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-07-11 11:41:45 +0630

    ss

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends ConsoleKernel
         //$schedule->command('session:twod-update-status')->oneDay();
         //$schedule->command('match:update-status')->daily();
         //$schedule->command('session:morning-status-open')->daily();
-         $schedule->command('session:morning-status-open')->dailyAt('11:40'); // Assuming this is when you want it to run
+         $schedule->command('session:morning-status-open')->dailyAt('11:43'); // Assuming this is when you want it to run
         $schedule->command('session:morning-prize-status-open')->daily();
         $schedule->command('session:morning-prize-status-close')->daily();
         $schedule->command('session:evening-status-open')->daily();
