2972dc233c8c8f30b33d129b6d1a65da0cbbc596
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2024-07-27 07:53:35 +0630

    2d dashboard

--- resources/views/admin/two_d/dashboard.blade.php
+++ resources/views/admin/two_d/dashboard.blade.php
@@ -65,34 +65,27 @@
           <div class="row mt-5">
             {{-- session two reset start 1 --}}
             <div class="col-lg-6 col-md-6 col-sm-6 mb-5">
-              <div class="card  mb-2 p-3">
+              {{-- <div class="card  mb-2 p-3">
                 <div class="d-flex mt-n2">
-                            <div class="avatar avatar-xl bg-info border-radius-xl p-2 mt-n4">
-                              <i class="fas fa-rotate fa-2x"></i>
-                                {{-- <img src="{{ asset('admin_app/assets/img/small-logos/logo-slack.svg') }}" alt="slack_logo"> --}}
+                          <div class="avatar avatar-xl bg-info border-radius-xl p-2 mt-n4">
+                              <i class="fas fa-rotate fa-2x"></i>   
                             </div>
                             <div class="ms-3 my-auto">
                                 <h6 class="mb-0"> 2D Session Reset</h6>
                                 <div class="avatar-group mt-4">
-                                    {{-- <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip"
-                                        data-original-title="Jessica Rowland">
-                                        <img alt="Image placeholder" src="{{ asset('admin_app/assets/img/team-3.jpg') }}"
-                                            class="">
-                                    </a> --}}
                                     <form action="{{ route('admin.SessionReset') }}" method="POST">
                                       @csrf
                                       <button class="btn btn-primary" type="submit">Reset</button>
                                   </form>
                                 </div>
                             </div>
-
                         </div>
 
                 <hr class="dark horizontal my-0">
                 <div class="card-footer p-3">
                   <p class="mb-0"><span class="text-success text-sm font-weight-bolder">ပွဲချိန်ပြီး တခုပြီးတိုင်း  </span>၁၅ မိနစ်အတွင်း လုပ်ပေးရပါမည်။</p>
                 </div>
-              </div>
+              </div> --}}
             </div>
             {{-- session reset 1 end --}}
           </div>
